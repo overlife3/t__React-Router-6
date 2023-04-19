ef9de73e0fe4060ab46975a7b6f0c20f5a21181a
overlife3 <vip.kostya.semenov.2004@mail.ru>
2023-04-20 00:04:45 +0300

    first commit

--- src/pages/Blogpage.tsx
+++ src/pages/Blogpage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Blogpage = () => {
-  return <h1>Blogpage</h1>;
+  return (
+    <div>
+      <h1>Blogpage</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non repellat
+      aspernatur quas expedita quisquam facilis ducimus ex nobis a voluptates.
+    </div>
+  );
 };
 
 export default Blogpage;
