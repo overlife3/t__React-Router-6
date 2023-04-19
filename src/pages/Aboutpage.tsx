8cbbb5959a9a82aeae51df31f63b393a3fc10f6f
overlife3 <vip.kostya.semenov.2004@mail.ru>
2023-04-19 23:52:32 +0300

    basics

---
+++ src/pages/Aboutpage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Aboutpage = () => {
+  return <h1>Aboutpage</h1>;
+};
+
+export default Aboutpage;
---
+++ src/pages/Notfoundpage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Notfoundpage = () => {
+  return (
+    <div>
+      Not found page. Go <Link to="/">home</Link>
+    </div>
+  );
+};
+
+export default Notfoundpage;
